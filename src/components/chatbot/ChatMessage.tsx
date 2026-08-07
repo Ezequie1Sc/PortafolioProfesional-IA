b27449722c1c37cc68da8e08f7e666c532b7102e
Ezequie1Sc <ezequielsc017@gmail.com>
2026-08-07 13:24:45 -0600

    feat: agregar dependecia y corregir chatmessage

--- src/components/chatbot/ChatMessage.tsx
+++ src/components/chatbot/ChatMessage.tsx
@@ -1,4 +1,6 @@
 import { Bot, User } from "lucide-react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import type { ChatMessage } from "../../types/chat";
 
 interface Props {
@@ -14,8 +16,10 @@ const ChatMessageItem = ({ message }: Props) => {
         {isUser ? <User size={20} /> : <Bot size={20} />}
       </div>
 
-      <div className="message-content">
-        {message.content}
+      <div className="message-content ai-message">
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
+          {message.content}
+        </ReactMarkdown>
       </div>
     </div>
   );
