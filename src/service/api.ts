2adf8efcc01b3e7fbdc11b9f93f146ef9a9b25ff
Ezequie1Sc <ezequielsc017@gmail.com>
2026-08-07 18:45:00 -0600

    feat: agregar api ts logica

---
+++ src/service/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_CHAT_API,
+  timeout: 60000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
